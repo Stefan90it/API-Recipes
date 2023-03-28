01e547102a02d26911d3b60c49ff29c894d0cab9
Stefan Petrovic <stefan90it@gmail.com>
2023-03-28 08:46:41 +0200

    Adding all projects to the GitHub

--- src/NencyKitchen.BusinessModel/BusinessEntites/RecipeStepResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NencyKitchen.BusinessModel.BusinessEntites
-{
-    public class RecipeStepResponse
-    {
-        public int RecipeStepId { get; set; }
-        public int StepNumber { get; set; }
-        public string Instruction { get; set; } = string.Empty;
-    }
-}
